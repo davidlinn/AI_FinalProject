bc65c5cc5e91e3f18e312341a57d719112d080aa
davidlinn <dlinn@hmc.edu>
2019-05-08 10:20:04 +0200

    Added new maps

--- student/config.py
+++ student/config.py
@@ -3,7 +3,7 @@ configuration = {
 "tile_size": 80, 
 "type": "load", #random
 "seed": None,
-"file": "./student/map.txt",
+"file": "./student/map_adv1.txt",
 "map_size": [10, 5],
 "delay": 0.1,
 "debugMap": False,
